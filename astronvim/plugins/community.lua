a55eb20f9f806acaafc5f464ccb0813b2eff54b7
zhsh9 <zhsh9@outlook.com>
2023-05-17 20:32:51 +0800

    change dotfiles for astronvim

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -6,7 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.lua" },
