c1a526e0cef3674925d159c0e0f6cb75c2260c0c
Shuhao Zhang <eclipse.tenet@gmail.com>
2023-05-31 01:20:57 +0800

    del curl and add fish config

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,9 @@
+function fish_prompt
+    # set_color $fish_color_cwd
+    # set_color normal
+    # echo -n (basename $PWD)
+    # set_color brcyan
+    set_color red
+    # echo -n '➤ '
+    echo -n '$ '
+end
